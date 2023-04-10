1a642517dad91ae42c39c710735a058db980a93c
Goette, Pablo <PGoette@lnw.com>
2023-04-10 12:21:38 -0300

    primer commit

---
+++ Hx.ArchiTesting/Roslyn/Utils/UnitTestUtil.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using SyntaxTree = Microsoft.CodeAnalysis.SyntaxTree;
+
+namespace Hx.ArchTests.Roslyn.Utils
+{
+    public class UnitTestUtil
+    {
+        public static readonly CSharpCompilationOptions DefaultCompilationOptions =
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+        
+        public static CSharpCompilation getCompilation(SyntaxNode node)
+        {
+            var directory = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+            var defaultReferences = Directory.GetFiles(directory).Where(s => s.EndsWith(".dll"))
+                .Select(s => MetadataReference.CreateFromFile(s));
+
+            var compilation = CSharpCompilation.Create("Test.dll", new SyntaxTree[] { node.SyntaxTree }, defaultReferences,
+                DefaultCompilationOptions);
+            return compilation;
+        }
+
+        public static SyntaxNode createNodeRoot(string content)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(content);
+            var nodeRoot = tree.GetRoot();
+            return nodeRoot;
+        }
+        
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ArchTestWithRoslyn
+A set of examples to use roslyn to test the application architecture
