a26d459d28ab9a944c965afa136c62c1b0a14074
Goette, Pablo <PGoette@lnw.com>
2023-04-14 07:42:54 -0300

    algunos tests

--- Roslyn/ArchiTesting.cs
+++ Roslyn/ArchiTesting.cs
@@ -1,6 +1,7 @@
 using Hx.ArchTests.Roslyn.Utils;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Hx.ArchTests.Roslyn;
 
@@ -8,23 +9,57 @@ public class ArchiTesting
 {
 
     [Test]
-    public void Should_Create_New_Variable_in_Class()
+    public void The_Parameters_Of_Service_Methods_Should_End_With_DTO()
     {
         // Arrange
-        var program = @" public class DeclarationExpressionsExample
-                                        {
-                                            public bool inputIsPositiveNumber = int.TryParse(ReadString(), out int input) && input > 0;
+        var program = @" public class ModelClassExample
+                         {
+                         } 
 
-                                            public static string ReadString() => Console.ReadLine();
-                                        }";
+                         public class ExampleService
+                         {
+                             public bool Test1Method() => true;
+                             public bool Test2Method(ModelClassExample example) => true;
+                         }";
 
         var nodeRoot = GetNodeRoot(program);
-        var compilation = UnitTestUtil.getCompilation(nodeRoot);
+        var classes = nodeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+            .Where(clazz => clazz.Identifier.ToString().EndsWith("Service")
+                            && clazz.DescendantNodes()
+                                .OfType<MethodDeclarationSyntax>().Any(method => method.ParameterList.Parameters.Any(
+                                    parameter => parameter.Identifier.ToString().EndsWith("DTO")
+                                )));
         
         // Assert
-        Assert.IsTrue(true);
+        Assert.IsTrue(!classes.Any());
     }
     
+    
+    [Test]
+    public void The_Parameters_Of_Service_Methods_Should_End_With_DTO2()
+    {
+        // Arrange
+        var program = @" public class ModelClassExample
+                         {
+                         } 
+
+                         public class ExampleService
+                         {
+                             public bool Test1Method() => true;
+                             public bool Test2Method(ModelClassExample exampleDTO) => true;
+                         }";
+
+        var nodeRoot = GetNodeRoot(program);
+        var classes = nodeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>()
+            .Where(clazz => clazz.Identifier.ToString().EndsWith("Service")
+            && clazz.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>().Any(method => method.ParameterList.Parameters.Any(
+                    parameter => parameter.Identifier.ToString().EndsWith("DTO")
+                )));
+        
+        // Assert
+        Assert.IsTrue(classes.Any());
+    }
 
     private SyntaxNode GetNodeRoot(string program) {
         var tree = CSharpSyntaxTree.ParseText(program);
