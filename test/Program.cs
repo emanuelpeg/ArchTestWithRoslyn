25fbfd728a226e6b8149662e4fe9e5e201e24b8f
Goette, Pablo <PGoette@lnw.com>
2023-05-19 07:06:50 -0300

    test

---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace com.hexacta.ArchTest.test;
+
+class Program
+{
+    private string greeting = "hola";
+
+    public Program(string greeting)
+    {
+        this.greeting = greeting;
+    }
+
+    public void CallHello()
+    {
+        Console.WriteLine(greeting);
+    }
+    
+    
+
+}
+
+
+
