335c65614136fe8abffa88261591bf6a60601baf
Goette, Pablo <PGoette@lnw.com>
2023-05-19 07:06:50 -0300

    test

--- Roslyn/ArchiTestingProject.cs
+++ Roslyn/ArchiTestingProject.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace Hx.ArchTests.Roslyn;
@@ -10,7 +8,7 @@ namespace Hx.ArchTests.Roslyn;
 public class ArchiTestingProject
 {
     [Test]
-    public async Task The_Parameters_Of_Service_Methods_Should_End_With_DTO()
+    public async Task The_Field_Private_Should_Start_With_Underscore()
     {
         // Arrange
         string projectPath = @"C:\projects\hat\ArchTestWithRoslyn\test\test.csproj";
@@ -24,12 +22,15 @@ public class ArchiTestingProject
                 var nodeRoot = syntaxTree.GetRoot();
                 var fields = nodeRoot.DescendantNodes()
                     .OfType<FieldDeclarationSyntax>()
-                    .Where(field => field.Modifiers.Any(modify => modify.Kind().Equals(SyntaxKind.PrivateKeyword))
-                    && field.Declaration.Variables.Any(aVar => !aVar.Identifier.ValueText.StartsWith("_"))
+                    .Where(field => field.Modifiers
+                                        .Any(modify => 
+                                            modify.Kind().Equals(SyntaxKind.PrivateKeyword))
+                    && field.Declaration.Variables
+                        .Any(aVar => !aVar.Identifier.ValueText.StartsWith("_"))
                     );
         
                 // Assert
-                Assert.IsTrue(fields.Any());
+                Assert.IsTrue(!fields.Any());
             }
 
         }
